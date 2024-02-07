1a6143d45d1408f964a0762f8eac6f236e6aaa84
EC2 Default User <ec2-user@ip-172-31-34-71.ap-south-1.compute.internal>
2024-02-07 09:16:41 +0000

    datatypes file got added

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+Class Demo{
+	public static main(String [] agrs){
+		int a=30;
+		int b=40;
+		System.out.println(a+b);
+		System x="hello";
+		System y="system";
+		System.out.println(a+b+x+y);
+	}
+
+}
+
